b7bc4c7871ab8cf758e45720410d6625a7cd7366
Dani_Exodus <ddenekew@hawk.iit.edu>
2018-11-21 19:56:16 -0600

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,7 +1,7 @@
-var map;
 function initMap() {
-  var bahirdar = center: {lat: 11.6, lng: 37.3833};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 8
-  }
-);
+  var map = {
+    center: new google.maps.LatLang(11.6, 37.3833),
+    zoom: 8,
+};
+  var map = new google.maps.Map(document.getElementById('googleMap'), map);
+}
