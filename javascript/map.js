87961f9a374e6e2e167598ff11c2746441f5df52
Dani_Exodus <ddenekew@hawk.iit.edu>
2018-11-27 13:57:03 -0600

    Update map.js

--- javascript/map.js
+++ javascript/map.js
@@ -1,16 +1,16 @@
 function initMap() {
-	var uluru = {lat: 41.9653, lng: 87.6577};
-	var loc1 = {lat: 41.9690 lng: 87.6599};
-  var loc2 = {lat: 41.9613, lng: 87.6534};
-  var loc3 = {lat: 41.9656, lng: 87.6501};
-  var loc4 = {lat: 41.9751, lng: 87.6550};
+	var loc = {lat: 41.9653, lng: -87.6577};
+	var loc1 = {lat: 41.9751, lng: -87.6550};
+  var loc2 = {lat: 41.9656, lng: -87.6501};
+  var loc3 = {lat: 41.9613, lng: -87.6534};
+  var loc4 = {lat: 41.9690, lng: -87.6599};
 
 	var map = new google.maps.Map(document.getElementById('map'), {
 		zoom: 4,
-		center: uluru
+		center: loc
 	});
 	var marker = new google.maps.Marker({
-		position: (uluru, loc1, loc2, loc3, loc4),
+		position: (loc, loc1, loc2, loc3, loc4),
 		map: map
 	});
 }
